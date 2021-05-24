97b0fc4fed58ab958b498f0eb3a66f1b0e5d561f
AJ <awilliamsonltx@gmail.com>
2021-05-24 13:31:54 -0500

    Updated 4.2.2 firmware to use Jyer's firmware

---
+++ BTT_TFT35_V3/copy_to_sd.bat
@@ -0,0 +1,5 @@
+MKDIR D:\TFT35
+xcopy TFT35 D:\TFT35 /e
+Copy config.ini D:\
+copy BIGTREE_TFT35_V3.0_E3.26.x.bin D:\
+pause
---
+++ BTT_TFT35_V3/rename_tft.bat
@@ -0,0 +1,6 @@
+D:
+REN TFT35.CUR TFT35
+ren BIGTREE_TFT35_V3.0_E3.26.x.cur BIGTREE_TFT35_V3.0_E3.26.x.bin
+ren config.ini.cur config.ini
+dir
+pause
